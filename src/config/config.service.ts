f7c0185831bfb1f18e75f9cb21049a7e20292c39
Erik Pizana <erikpzcanedo@outlook.com>
2021-06-08 16:56:08 -0500

    First commit and user module

---
+++ src/config/config.service.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+import { parse } from 'dotenv';
+
+export class ConfigService {
+  private readonly envConfig: {
+    [key: string]: string;
+  };
+
+  constructor() {
+    const isDevelopmentEnv = process.env.NODE_ENV !== 'production';
+    if (isDevelopmentEnv) {
+      const envFilePath = __dirname + '/../../.env';
+      const existsPath = fs.existsSync(envFilePath);
+      if (!existsPath) {
+        console.log('.env file no existe');
+        process.exit(0);
+      }
+      this.envConfig = parse(fs.readFileSync(envFilePath));
+    } else {
+      PORT: process.env.PORT;
+    }
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+}
