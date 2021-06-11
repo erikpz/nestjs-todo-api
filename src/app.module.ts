c4f79adc46c5ba5e433c6e5d151b3c4cdac9944c
Erik Pizana <erikpzcanedo@outlook.com>
2021-06-10 20:00:53 -0500

    port final

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,12 @@ import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { Configuration } from './config/config.keys';
 import { MongooseModule } from '@nestjs/mongoose';
-require('dotenv').config()
+require('dotenv').config();
 @Module({
   imports: [
     UserModule,
     ConfigModule,
-    MongooseModule.forRoot(
-      process.env.MONGO_URI
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
 })
-export class AppModule {
-  static port: number | string;
-  constructor(private readonly _configService: ConfigService) {
-    AppModule.port = this._configService.get(process.env.PORT);
-  }
-}
+export class AppModule {}
